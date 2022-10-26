94d917ba48bff7c9c4b97b6c3502f1372b20381c
Lais Omena <laisf.omena@gmail.com>
2022-10-26 18:19:59 +0100

    Requisito 10, 11 e 12

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
@@ -9,6 +9,8 @@ function MyProvider({ children }) {
   const [ingredient, setIngredient] = useState(false);
   const [name, setName] = useState(false);
   const [letter, setLetter] = useState(false);
+  /*   const [dataFood, setDataFood] = useState([]);
+  const [dataDrink, setDataDrink] = useState([]); */
 
   const hideSearchBar = () => {
     setHideSearchInput(!hideSearchInput);
@@ -43,41 +45,81 @@ function MyProvider({ children }) {
         const responseIngredientsFood = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${food}`);
         const dataIngredientsFood = await responseIngredientsFood.json();
 
-        const responseIngredientsDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drink}`);
-        const dataIngredientsDrink = await responseIngredientsDrink.json();
+        console.log(dataIngredientsFood);
 
-        console.log(dataIngredientsFood, dataIngredientsDrink);
-        return (dataIngredientsFood, dataIngredientsDrink);
+        setDataFood(dataIngredientsFood);
+
+        return dataIngredientsFood;
       } if (name) {
         const responseNameFood = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`);
         const dataNameFood = await responseNameFood.json();
 
-        const responseNameDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`);
-        const dataNameDrink = await responseNameDrink.json();
+        setDataFood(dataNameFood);
 
-        console.log(dataNameFood, dataNameDrink);
-        return (dataNameFood, dataNameDrink);
+        return dataNameFood;
       }
       if (letter && food.length === 1) {
         const responseFirstLetterFood = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${food[0]}`);
         const dataFirstLetterFood = await responseFirstLetterFood.json();
 
-        const responseFirstLetterDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${drink[0]}`);
-        const dataFirstLetterDrink = await responseFirstLetterDrink.json();
+        setDataFood(dataFirstLetterFood);
 
-        console.log(dataFirstLetterFood, dataFirstLetterDrink);
-        return (dataFirstLetterFood, dataFirstLetterDrink);
+        return dataFirstLetterFood;
       }
       return global.alert('Your search must have only 1 (one) character');
     } catch (e) {
       throw new Error(e);
     }
-  }, [drink, food, ingredient, letter, name]);
+  }, [food, ingredient, letter, name]);
+
+  const filterDrink = useCallback(async () => {
+    try {
+      if (ingredient) {
+        const responseIngredientsDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${drink}`);
+        const dataIngredientsDrink = await responseIngredientsDrink.json();
+
+        setDataDrink(dataIngredientsDrink);
+        console.log(dataIngredientsDrink);
+        return dataIngredientsDrink;
+      } if (name) {
+        const responseNameDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`);
+        const dataNameDrink = await responseNameDrink.json();
+
+        setDataDrink(dataNameDrink);
+
+        return dataNameDrink;
+      }
+      if (letter && drink.length === 1) {
+        const responseFirstLetterDrink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${drink[0]}`);
+        const dataFirstLetterDrink = await responseFirstLetterDrink.json();
+
+        setDataDrink(dataFirstLetterDrink);
+
+        return dataFirstLetterDrink;
+      }
+      return global.alert('Your search must have only 1 (one) character');
+    } catch (e) {
+      throw new Error(e);
+    }
+  }, [drink, ingredient, letter, name]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     filterFood();
+    filterDrink();
   };
+
+  /*   const verifyDataSizeFood = () => {
+    if (dataFood.length === 1) {
+      return dataFood.idMeal;
+    }
+  };
+
+  const verifyDataSizeDrink = () => {
+    if (dataDrink.length === 1) {
+      return dataDrink.idDrink;
+    }
+  }; */
 
   const context = useMemo(() => ({ hideSearchInput,
     setHideSearchInput,

e1906e78acf8fb06a7f89a06ff10dca5e13ab520
JoseFilipeVieiraFurieri <jf.furieri@ymail.com>
2022-10-29 18:21:16 -0300

    requisito 26 e 27

--- src/pages/DrinkId.jsx
+++ src/pages/DrinkId.jsx
@@ -1,15 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import RecipeDetails from '../components/RecipeDetails';
 import MyContext from '../context/MyContext';
+import './mealsDrinks.css';
 
 export default function DrinkId() {
-  const { drink } = useContext(MyContext);
+  const { recoMeals, drink, setRecoMeals } = useContext(MyContext);
+  const MAX = 6;
+
+  const handleAPI = async () => {
+    const request = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const JSON = await request.json();
+    setRecoMeals(JSON.meals);
+  };
+
+  useEffect(() => {
+    handleAPI();
+  }, []);
 
   return (
     <div>
       <RecipeDetails />
       DrinkId
       {drink}
+      <div className="carrosel">
+        { recoMeals?.filter((x, i) => i < MAX)
+          .map((e, index) => (
+            <div
+              key={ index }
+              data-testid={ `${index}-recommendation-card` }
+              className="card"
+            >
+              <p data-testid={ `${index}-recommendation-title` }>{ e.strMeal }</p>
+              <div className="image">
+                <img src={ e.strMealThumb } alt={ e.strMeal } />
+              </div>
+            </div>))}
+      </div>
     </div>
   );
 }
--- src/pages/MealId.jsx
+++ src/pages/MealId.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import RecipeDetails from '../components/RecipeDetails';
+import MyContext from '../context/MyContext';
+import './mealsDrinks.css';
 
 export default function MealId() {
+  const { recoDrinks, setRecoDrinks } = useContext(MyContext);
+  const MAX = 6;
+
+  const handleAPI = async () => {
+    const request = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const JSON = await request.json();
+    setRecoDrinks(JSON.drinks);
+  };
+
+  useEffect(() => {
+    handleAPI();
+  }, []);
+
   return (
     <div>
       MealId
       <RecipeDetails />
+      <div className="carrosel">
+        { recoDrinks?.filter((x, i) => i < MAX)
+          .map((e, index) => (
+            <div
+              key={ index }
+              data-testid={ `${index}-recommendation-card` }
+              className="card"
+            >
+              <p data-testid={ `${index}-recommendation-title` }>{ e.strDrink }</p>
+              <div className="image">
+                <img src={ e.strDrinkThumb } alt={ e.strDrink } />
+              </div>
+            </div>))}
+      </div>
     </div>
   );
 }
